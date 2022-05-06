0d0cb4806abb3ac83c6a2453f4f78d15dbac914e
Ulad <debonairartist@gmail.com>
2022-05-06 16:34:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let increment = 10;
+increment++;
+increment = increment + 1;
+increment = increment * 3;
+increment = (-increment) ** 2;
+
+
+const o = {
+    lol: 2,
+    vasya: 3,
+    pedro: "name"
+};
+
+function multiply(a, b) {
+    return a * b
+};
+
+
+
+console.log(
+    multiply(8, 3)
+);
